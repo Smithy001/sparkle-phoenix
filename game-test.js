fe5578395d947f91a76bbf6585f8af32b5a480c1
Smithy01 <>
2023-06-23 07:38:16 -0700

    Add game-test

---
+++ game-test.js
@@ -0,0 +1,43 @@
+const Game = require('./src/game');
+
+/*
+Direction = value * 45
+
+0 => 0 // Up
+1 => 45
+2 => 90 // Right
+3 => 135
+4 => 180 // Down
+5 => 225
+6 => 270 // Left
+7 => 315
+*/
+
+var game = new Game();
+
+game.newGame(3);
+
+game.playerJoin('abc')
+game.playerJoin('cbd')
+game.playerJoin('edg')
+
+game.endTurn('abc', 0, 0)
+game.endTurn('cbd', 5, 7)
+game.endTurn('edg', 1, 4)
+
+game.endTurn('abc', 0, 0)
+game.endTurn('cbd', 5, 7)
+game.endTurn('edg', 1, 4)
+
+game.endTurn('abc', 0, 0)
+game.endTurn('cbd', 5, 7)
+game.endTurn('edg', 1, 4)
+
+// Player abc dies
+
+game.endTurn('cbd', 5, 7)
+game.endTurn('edg', 1, 4)
+
+// Player cbd dies
+
+// Player edg wins
---
+++ src/game.js
@@ -0,0 +1,49 @@
+// gameBoard.js
+
+class GameBoard {
+    constructor() {
+      this.board = this.createBoard();
+    }
+  
+    createBoard() {
+      const board = [];
+      for (let row = 0; row < 10; row++) {
+        const rowData = [];
+        for (let col = 0; col < 10; col++) {
+          rowData.push({ ship: null, bullet: null });
+        }
+        board.push(rowData);
+      }
+      return board;
+    }
+  
+    placePiece(row, col, piece, direction) {
+      if (row < 0 || row >= 10 || col < 0 || col >= 10) {
+        throw new Error('Invalid position on the game board.');
+      }
+  
+      if (piece === 'ship') {
+        this.board[row][col].ship = { direction };
+      } else if (piece === 'bullet') {
+        this.board[row][col].bullet = { direction };
+      } else {
+        throw new Error('Invalid piece type. Only "ship" and "bullet" are allowed.');
+      }
+    }
+  
+    getPiece(row, col, piece) {
+      if (row < 0 || row >= 10 || col < 0 || col >= 10) {
+        throw new Error('Invalid position on the game board.');
+      }
+  
+      if (piece === 'ship') {
+        return this.board[row][col].ship;
+      } else if (piece === 'bullet') {
+        return this.board[row][col].bullet;
+      } else {
+        throw new Error('Invalid piece type. Only "ship" and "bullet" are allowed.');
+      }
+    }
+  }
+  
+  module.exports = GameBoard;
